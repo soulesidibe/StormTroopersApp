02a696ce32a90fe4d3e3eea803e4cbd9e0b6477f
Souleymane Sidibe <souleymane.sidibe01@gmail.com>
2018-11-24 19:07:53 +0000

    first commit

---
+++ app/src/main/java/com/soulesidibe/stormtroopersapp/LastTripsActivity.kt
@@ -0,0 +1,12 @@
+package com.soulesidibe.stormtroopersapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LastTripsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last_trips)
+    }
+}
